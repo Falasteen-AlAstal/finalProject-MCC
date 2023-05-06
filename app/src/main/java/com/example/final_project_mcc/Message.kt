8ca3e4361ef9af92988647daa0dd343a88dc4c94
Falasteen-AlAstal <113950375+Falasteen-AlAstal@users.noreply.github.com>
2023-05-06 18:45:52 +0300

    User interfaces

---
+++ app/src/main/java/com/example/final_project_mcc/Message.kt
@@ -0,0 +1,10 @@
+package com.example.final_project_mcc
+
+
+data class Message(
+    val text: String = "",
+    val senderId: String = "",
+    val receiverId :String = "",
+    val timestamp: Long = 0
+
+)
