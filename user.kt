f32184a9c33b9da09cabb046b5de930209788ca6
balsamsrhan <125121061+balsamsrhan@users.noreply.github.com>
2023-05-09 21:12:35 +0300

    Delete user.kt

--- user.kt
+++
@@ -1,3 +0,0 @@
-package com.example.final_project_mcc
-
-class user (var address:String? =null ,var dateBirth:String? =null,var email:String? =null ,var firstName :String? =null, var id :String? =null,var lastName :String? =null,var middleName:String? =null , var password:String? =null,var phone:String? =null ,var role:String? =null)
